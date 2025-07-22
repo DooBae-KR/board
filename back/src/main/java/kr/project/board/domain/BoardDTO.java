e1911a8cb52f8754281a83f42aed08a5504ac03a
DooBae-KR <DOOBAE-KR>
2025-07-22 11:23:23 +0900

    깃 이그노어 변경

---
+++ back/src/main/java/kr/project/board/domain/BoardDTO.java
@@ -0,0 +1,13 @@
+package dto;
+
+import lombok.Data;
+
+@Data
+public class BoardDTO {
+    private int boardIdx;
+    private String boardTitle;
+    private String boardContent;
+    private String boardCreateTime;
+    private String boardUpdateTime;
+    private Boolean del;    
+}
